81bf569ef992d83964b6f12a33c184121e3c6d17
Martin Artishevsky <artishevsky.m@gmail.com>
2021-04-22 16:54:14 +0100

    Complete the course detail page

--- lib/ui/course_detail/course_detail_page.dart
+++ lib/ui/course_detail/course_detail_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rw_courses/model/course.dart';
+import 'package:flutter_rw_courses/ui/course_detail/image_container.dart';
+import 'package:flutter_rw_courses/utils/string_extensions.dart';
 
 class CourseDetailPage extends StatelessWidget {
   final Course course;
@@ -12,7 +14,73 @@ class CourseDetailPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(course.name),
       ),
-      body: Text('Course detail'),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          _buildBanner(),
+          _buildMain(context),
+          _buildDetails(context),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildBanner() {
+    return ImageContainer(
+      height: 200,
+      url: course.artworkUrl,
+    );
+  }
+
+  Widget _buildMain(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            course.name,
+            style: Theme
+                .of(context)
+                .textTheme
+                .subtitle2
+                .copyWith(fontSize: 24),
+          ),
+          Text(
+            course.description,
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w300),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDetails(BuildContext context) {
+    final style = TextStyle(fontSize: 16);
+
+    return Padding(
+      padding: const EdgeInsets.only(left: 10.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            'Domain(s): ${course.domainString}',
+            style: style,
+          ),
+          Text(
+            'Level: ${course.difficulty.capitalize()}',
+            style: style,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0),
+            child: Text(
+              '${course.contributors}',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
