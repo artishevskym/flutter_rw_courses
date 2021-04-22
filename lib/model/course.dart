10f11f6913c1efefed41537b52ead58ab9b61392
Martin Artishevsky <artishevsky.m@gmail.com>
2021-04-22 12:33:42 +0100

    Challenge - add more properties

--- lib/model/course.dart
+++ lib/model/course.dart
@@ -2,16 +2,23 @@ class Course {
   final String courseId;
   final String name;
   final String description;
+  final String artworkUrl;
+  final String difficulty;
+  final String contributors;
 
-  Course(this.courseId, this.name, this.description);
+  Course(this.courseId, this.name, this.description, this.artworkUrl,
+      this.difficulty, this.contributors);
 
   Course.fromJson(Map<String, dynamic> json)
       : courseId = json["id"],
         name = json["attributes"]["name"],
-        description = json["attributes"]["description_plain_text"];
+        description = json["attributes"]["description_plain_text"],
+        artworkUrl = json["attributes"]["card_artwork_url"],
+        difficulty = json["attributes"]["difficulty"],
+        contributors = json["attributes"]["contributor_string"];
 
   @override
   String toString() {
-    return name;
+    return name + ": " + difficulty + ": " + contributors;
   }
 }
