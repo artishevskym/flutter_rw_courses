4d24f59d9e0c4125bb6b8ebc3b586692dfcd42e7
Martin Artishevsky <artishevsky.m@gmail.com>
2021-04-22 21:25:45 +0100

    Lock app to portrait mode

--- lib/rwcourses_app.dart
+++ lib/rwcourses_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_rw_courses/strings.dart';
 import 'package:flutter_rw_courses/ui/courses/couses_page.dart';
 import 'package:flutter_rw_courses/ui/filter/filter_page.dart';
@@ -6,6 +7,7 @@ import 'package:flutter_rw_courses/ui/filter/filter_page.dart';
 class RWCoursesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     return Scaffold(
       appBar: AppBar(
         title: Text(Strings.appTitle),
