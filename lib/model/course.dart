11fb646ae4177cc71c0ccd3daf40a6e398d1b417
Martin Artishevsky <artishevsky.m@gmail.com>
2021-04-22 12:58:37 +0100

    Parse domains

--- lib/model/course.dart
+++ lib/model/course.dart
@@ -1,3 +1,6 @@
+import '../constants.dart';
+import 'domain.dart';
+
 class Course {
   final String courseId;
   final String name;
@@ -5,9 +8,10 @@ class Course {
   final String artworkUrl;
   final String difficulty;
   final String contributors;
+  final List<Domain> domains;
 
   Course(this.courseId, this.name, this.description, this.artworkUrl,
-      this.difficulty, this.contributors);
+      this.difficulty, this.contributors, this.domains);
 
   Course.fromJson(Map<String, dynamic> json)
       : courseId = json["id"],
@@ -15,10 +19,50 @@ class Course {
         description = json["attributes"]["description_plain_text"],
         artworkUrl = json["attributes"]["card_artwork_url"],
         difficulty = json["attributes"]["difficulty"],
-        contributors = json["attributes"]["contributor_string"];
+        contributors = json["attributes"]["contributor_string"],
+        domains = [] {
+    var domainData = json["relationships"]["domains"]["data"] as List<dynamic>;
+    if (domainData != null && domainData.length > 0) {
+      for (var i = 0; i < domainData.length; i++) {
+        var domain =
+            Course.getDomain(json["relationships"]["domains"]["data"][i]["id"]);
+        domains.add(domain);
+      }
+    }
+  }
+
+  static Domain getDomain(String domainId) {
+    switch (domainId) {
+      case Constants.iosDomain:
+        return Domain.ios;
+      case Constants.androidDomain:
+        return Domain.android;
+      case Constants.unityDomain:
+        return Domain.unity;
+      case Constants.sssDomain:
+        return Domain.sss;
+      case Constants.flutterDomain:
+        return Domain.flutter;
+      case Constants.macOsDomain:
+        return Domain.macos;
+      case Constants.archiveDomain:
+        return Domain.archived;
+      default:
+        return Domain.unknown;
+    }
+  }
+
+  get domainString {
+    String result = "";
+    for (var i = 0; i < domains.length - 1; i++) {
+      result += domains[i].name + ", ";
+    }
+    result += domains.last.name;
+    return result;
+  }
 
   @override
   String toString() {
-    return name + ": " + difficulty + ": " + contributors;
+    return name + ": " + domainString;
   }
 }
---
+++ lib/model/domain.dart
@@ -0,0 +1,28 @@
+import '../strings.dart';
+
+enum Domain { all, ios, android, flutter, sss, unity, macos, archived, unknown }
+
+extension DomainExtension on Domain {
+  String get name {
+    switch (this) {
+      case Domain.ios:
+        return Strings.ios;
+      case Domain.all:
+        return Strings.all;
+      case Domain.android:
+        return Strings.android;
+      case Domain.flutter:
+        return Strings.flutter;
+      case Domain.sss:
+        return Strings.sss;
+      case Domain.unity:
+        return Strings.unity;
+      case Domain.macos:
+        return Strings.macOS;
+      case Domain.archived:
+        return Strings.archived;
+      default:
+        return Strings.unknown;
+    }
+  }
+}
