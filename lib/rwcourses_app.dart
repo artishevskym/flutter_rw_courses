2b766b3aa4042feb5293a06d6e5af0378fcf1122
Martin Artishevsky <artishevsky.m@gmail.com>
2021-04-22 17:01:52 +0100

    Challenge - add filter page

--- lib/rwcourses_app.dart
+++ lib/rwcourses_app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rw_courses/strings.dart';
 import 'package:flutter_rw_courses/ui/courses/couses_page.dart';
+import 'package:flutter_rw_courses/ui/filter/filter_page.dart';
 
 class RWCoursesApp extends StatelessWidget {
   @override
@@ -8,6 +9,16 @@ class RWCoursesApp extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(Strings.appTitle),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.filter_list),
+            onPressed: () => Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (_) => FilterPage(),
+              ),
+            ),
+          )
+        ],
       ),
       body: CoursesPage(),
     );
---
+++ lib/ui/filter/filter_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import '../../strings.dart';
+
+class FilterPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _FilterPageState();
+}
+
+class _FilterPageState extends State<FilterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(Strings.filter),
+      ),
+      body: Text('Filter'),
+    );
+  }
+}
